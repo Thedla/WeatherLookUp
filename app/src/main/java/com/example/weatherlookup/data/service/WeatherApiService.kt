12bcd719ff77011bf8c96b108e6a6ade1a9fbe9e
Sujith Thedla <sujiththedla050@gmail.com>
2025-07-09 17:12:59 -0400

    Added appid comment

--- app/src/main/java/com/example/weatherlookup/data/service/WeatherApiService.kt
+++ app/src/main/java/com/example/weatherlookup/data/service/WeatherApiService.kt
@@ -8,6 +8,7 @@ interface WeatherApiService {
     @GET("data/2.5/forecast")
     suspend fun getForecast(
         @Query("q") city: String,
+        // Need to handle differently in Production environment for simplicity i'm directly using the api key
         @Query("appid") apiKey: String = "65d00499677e59496ca2f318eb68c049"
     ): WeatherResponse
 }
